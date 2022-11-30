ddc14c8e205adfe46ec4647f31cf15a4c42f9fa6
Gildo Neto <gildo.neto@telus.com>
2022-11-30 15:25:16 -0300

    deleting

--- src/0205-arrays/ex01.ts
+++
@@ -1,9 +0,0 @@
-async function fetchCursos() {
-  const response = await fetch('https://api.origamid.dev/json/cursos.json');
-  const data = await response.json();
-  mostrarCursos(data);
-}
-
-fetchCursos();
-
-function mostrarCursos(cursos) {}
